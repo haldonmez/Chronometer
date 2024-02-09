6118cd1e21615c564d3608b2e5a3fb501e728484
HalilDnmZ <123744643+haldonmez@users.noreply.github.com>
2024-02-09 16:39:14 +0300

    Save Button

--- main.js
+++ main.js
@@ -8,17 +8,27 @@ function startChronoMeter() {
     stopwatchInterval = setInterval(updateStopwatch, 1000); // update every second
   }
 }
-
 function stopChronoMeter() {
     clearInterval(stopwatchInterval); // stop the interval
-    elapsedPausedTime = new Date().getTime() - startTime; // calculate elapsed paused time
-    stopwatchInterval = null; // reset the interval variable
+    if(startTime==undefined) // Takes care of the "NULL" first time the chronometer is started.
+    {
+      startFirstTime = new Date().getTime()
+      elapsedPausedTime = new Date().getTime() - startFirstTime; // calculate elapsed paused time
+      stopwatchInterval = null;
+    }
+    else
+    {
+      elapsedPausedTime = new Date().getTime() - startTime; // calculate elapsed paused time
+      stopwatchInterval = null; // reset the interval variable
+    }
+    
   }
 
 function resetChronoMeter() {
     stopChronoMeter(); // stop the interval
     elapsedPausedTime = 0; // reset the elapsed paused time variable
-    document.getElementById("timer").innerHTML = "00:00:00"; // reset the display
+    document.getElementById("timer").innerHTML = "00:00:00";
+    document.getElementById("icon").innerHTML = "00:00:00"; // reset the display
   }
 
 function updateStopwatch() {
@@ -29,9 +39,19 @@ function updateStopwatch() {
     var hours = Math.floor(elapsedTime / 1000 / 60 / 60); // calculate hours
     var displayTime = pad(hours) + ":" + pad(minutes) + ":" + pad(seconds); // format display time
     document.getElementById("timer").innerHTML = displayTime; // update the display
+    document.getElementById("icon").innerHTML = displayTime; // update the display
 }
   
 function pad(number) {
     // add a leading zero if the number is less than 10
     return (number < 10 ? "0" : "") + number;
+}
+
+function saveChronoMeter()
+{
+  const newLabel = document.createElement('p');
+  newLabel.textContent = 'Click me!';
+  newLabel.style.color = 'white';
+  document.body.appendChild(newLabel);
+
 }
