125f05e3b7f49e0134c776581a96e30b4549ea92
HalilDnmZ <123744643+haldonmez@users.noreply.github.com>
2024-02-09 12:02:48 +0300

    functions

---
+++ main.js
@@ -0,0 +1,7 @@
+var startTime; // to keep track of the start time
+var stopwatchInterval; // to keep track of the interval
+var elapsedPausedTime = 0; // to keep track of the elapsed time while stopped
+
+function startChronoMeter() {
+    alert("hey");
+  }
