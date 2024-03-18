33b60e3d561897a0df38c00e9a3249a148111655
HalilDnmZ <123744643+haldonmez@users.noreply.github.com>
2024-03-18 16:26:14 +0300

    Notify Button Changes
    Notify Button menu created
    Dropdown menu's will be adjusted

--- main.js
+++ main.js
@@ -99,3 +99,41 @@ function takeScreenShot()
       a.click();
     })
 }
+
+
+
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("notifyButton");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+// Handle form submission
+document.getElementById("myForm").addEventListener("submit", function(event){
+  event.preventDefault();
+  // Get the form data
+  var formData = new FormData(event.target);
+  // Use the form data
+  console.log(formData.get("fname"));
+  // Close the modal
+  modal.style.display = "none";
+});
